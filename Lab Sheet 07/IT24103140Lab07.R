5986f8bf321d0fed1bafca8c7b0ae66ab07b99f0
IT24103140 <it24103140@my.sliit.lk>
2025-09-16 16:24:19 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103140Lab07.R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\Dinali\\OneDrive - Sri Lanka Institute of Information Technology\\Desktop\\IT24103140")
+
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+1 - pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
